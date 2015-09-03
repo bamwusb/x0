011bd6c88ee068f925f87b6ca975dcc6495dcfe5
Demo github student account "bamwusb" <bam@wusb.fm>
2015-09-02 20:49:26 -0400

    yet another 93)
    blah blah

--- circles0.java
+++ circles0.java
@@ -2,13 +2,16 @@
 
 // Setup:  screen size. //
 void setup() {
-  size(200, 300);                // Try changing the width & height.
+  size(200, 400);                // Try changing the width & height.
 }
 
 // Next frame:  circle at (x,y). //
 void draw() {
-  ellipse( 50,50, 60,90 );       // Try changing the (x,y) and size.
+  ellipse( 60,50, 60,90 );       // Try changing the (x,y) and size.
 
-  text( "Ho ho!", 50, height-50 );
+  text( "Hee hee!", 50, height-50 );
 
 }
+
+
+// comment
