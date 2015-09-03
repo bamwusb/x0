c5e419b8edec7b8e62cc17cd44994cd2e35f6884
Demo github student account "bamwusb" <bam@wusb.fm>
2015-09-02 20:42:45 -0400

    Ha-ha changes
    I added some text,
    and messed up the circle.

--- circles0.java
+++ circles0.java
@@ -2,10 +2,13 @@
 
 // Setup:  screen size. //
 void setup() {
-  size(200, 200);                // Try changing the width & height.
+  size(200, 300);                // Try changing the width & height.
 }
 
 // Next frame:  circle at (x,y). //
 void draw() {
-  ellipse( 50,50, 30,30 );       // Try changing the (x,y) and size.
+  ellipse( 50,50, 60,30 );       // Try changing the (x,y) and size.
+
+  text( "Ha ha!", 50, height-20 );
+
 }
